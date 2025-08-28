427f900cab68e8b3267e8eb8ff8ad15a9e0044fa
kahenoz <ozturk.kahen@gmail.com>
2025-08-28 14:18:38 +0200

    feat(models): add VehicleDB model for vehicles table

---
+++ model.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+class VehicleDB(Base):
+    __tablename__ = 'vehicles'
+    brand = Column(String, primary_key=True)
+    gate_id = Column(Integer)
+    VIN = Column(String)
